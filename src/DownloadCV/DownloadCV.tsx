7c211a2c72caf69fc8548e099fcdce2190268385
duydinhnguyen <57514892+duydinhnguyen@users.noreply.github.com>
2023-02-03 10:57:11 +0700

    update

---
+++ src/DownloadCV/DownloadCV.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './DownloadCV.module.scss'
+
+function DownloadCV() {
+  return (
+    <div className={styles['downloadCV']}>
+      <div className={styles['btnDownload']}>
+        <p>Download Cv</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default DownloadCV
--- src/WelcomeIntro/WelcomeIntro.tsx
+++ src/WelcomeIntro/WelcomeIntro.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import DownloadCV from '../DownloadCV/DownloadCV'
+import styles from './WelcomeIntro.module.scss'
 
 function WelcomeIntro() {
   return (
-    <div>WelcomeIntro</div>
+    <div className={styles['WelcomeIntro']}>
+       <div className={styles['WelcomeIntroContainer']}>
+          <p className={styles['nameAuthor']}>Hello, I am Nguyen Dinh Duy</p>
+          <p className={styles['job']}>I am developer</p>
+          <p className={styles['detailYourself']}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque iure quae, ipsa blanditiis magnam provident sit totam voluptatibus reiciendis at commodi non fugit, voluptate expedita dignissimos nostrum odio ex incidunt.</p>
+          <DownloadCV/>
+       </div>
+    </div>
   )
 }
 
